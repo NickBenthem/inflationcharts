06856f29aa86bbbaa5851c9ff138118851d791c0
Nick Benthem <nick@benthem.io>
2021-05-12 21:27:25 -0400

    have something - but hard to see everything - lets find a better solution

--- dash-oil-and-gas/scratch_explore.py
+++ dash-oil-and-gas/scratch_explore.py
@@ -1,3 +1,8 @@
 import pandas as pd
 
-df_explore = pd.read_csv(r"C:\git\bls\data\cu\cu.data.0.Current")
+df_explore = pd.read_csv(r"C:\git\bls\data\cu\cu.data.0.Current", sep="\t")
+df_items = pd.read_csv(r"C:\git\bls\data\cu\cu.item", sep="\t")
+current_period = df_explore.query("year == 2021 & period =='M04'")
+
+
+
