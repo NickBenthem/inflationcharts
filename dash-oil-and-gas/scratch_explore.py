fd83c07c3f1ef1d550c4cd2b6b831159888b1556
Nick Benthem <nick@benthem.io>
2021-05-12 21:37:50 -0400

    a thing that works - we can use this to start SQL

--- dash-oil-and-gas/scratch_explore.py
+++ dash-oil-and-gas/scratch_explore.py
@@ -1,8 +1,25 @@
 import pandas as pd
+import sqlite3 as sql
+
+import sqlalchemy as sqlalchemy
 
 df_explore = pd.read_csv(r"C:\git\bls\data\cu\cu.data.0.Current", sep="\t")
-df_items = pd.read_csv(r"C:\git\bls\data\cu\cu.item", sep="\t")
+df_series = pd.read_csv(r"C:\git\bls\data\cu\cu.series", sep="\t")
 current_period = df_explore.query("year == 2021 & period =='M04'")
 
 
 
+sqlite = sqlalchemy.pool.manage(sql, poolclass=sqlalchemy.pool.StaticPool )
+conn = sqlite.connect(':memory:')
+
+
+df_explore.to_sql('current_data',conn)
+df_series.to_sql('series',conn)
+
+test =  pd.read_sql("""
+                        SELECT * FROM current_data
+                                    LEFT JOIN items on current_data.series_id = 
+                    """
+
+
+                    , conn)
