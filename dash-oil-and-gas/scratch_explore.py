28ec342d99ea8166957d7d27792f63909f9cec1e
Nick Benthem <nick@benthem.io>
2021-05-12 23:40:11 -0400

    We need these updates for later. Youll see why

--- dash-oil-and-gas/scratch_explore.py
+++ dash-oil-and-gas/scratch_explore.py
@@ -6,34 +6,35 @@ import sqlite3 as sql
 import sqlalchemy as sqlalchemy
 
 # https://www.bls.gov/cpi/data.htm
-df_explore = pd.read_csv(r"C:\git\bls\data\cu\cu.data.0.Current", sep="\t") # Adding the nrows to go faster. This is big and Pycharm doesn't like it.
-df_series = pd.read_csv(r"C:\git\bls\data\cu\cu.series", sep="\t")
+def read_bls_df(filepath):
+    df = pd.read_csv(filepath,sep="\t")
+    df_obj = df.select_dtypes(['object'])
+    df[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
+    df.columns = df.columns.str.strip()
 
-df_obj = df_series.select_dtypes(['object'])
-df_explore_obj = df_explore.select_dtypes(['object'])
-# https://stackoverflow.com/questions/40950310/strip-trim-all-strings-of-a-dataframe
-df_series[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
-df_explore[df_explore_obj.columns] = df_explore_obj.apply(lambda x: x.str.strip())
+    return df
 
-# https://stackoverflow.com/questions/21606987/how-can-i-strip-the-whitespace-from-pandas-dataframe-headers
-df_series.columns = df_series.columns.str.strip()
-df_explore.columns = df_explore.columns.str.strip()
+df_explore = read_bls_df(r"C:\git\bls\data\cu\cu.data.0.Current") # Adding the nrows to go faster. This is big and Pycharm doesn't like it.
+df_series = read_bls_df(r"C:\git\bls\data\cu\cu.series")
+df_items = read_bls_df(r"C:\git\bls\data\cu\cu.item")
 
-# current_period = df_explore.query("year == 2021 & period =='M04'")
 
 sqlite = sqlalchemy.pool.manage(sql, poolclass=sqlalchemy.pool.StaticPool )
 conn = sqlite.connect(':memory:')
 
 
-df_explore.to_sql('current_data',conn)
-df_series.to_sql('series',conn)
+df_explore.to_sql('current_data', conn)
+df_series.to_sql('series', conn)
+df_items.to_sql('items', conn)
 
 test =  pd.read_sql("""
-                        SELECT current_data.value, series.* FROM current_data
+                        SELECT current_data.value, items.item_name, series.begin_year || '-' || substr(series.begin_period,2,4) as date
+                         FROM current_data
                                     LEFT JOIN series on series.series_id = current_data.series_id
+                                    LEFT JOIN items on series.item_code = items.item_code
                     """
                     , conn)
 
-test.to_csv(r"C:\git\bls\dash-oil-and-gas\data\bls_joined_data.csv")
+test.to_csv(r"C:\git\bls\dash-inflation\bls_joined_data.csv",index=False)
 
 print(f"runtime is {timeit.default_timer() - starttime}")
