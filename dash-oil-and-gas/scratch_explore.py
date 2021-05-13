104bc71a70f6c954dfd484aa4d4d3e9b7ef5487a
Nick Benthem <nick@benthem.io>
2021-05-12 21:54:50 -0400

    Well get back to the import, but for now we have working current data. Thats cool

--- dash-oil-and-gas/scratch_explore.py
+++ dash-oil-and-gas/scratch_explore.py
@@ -4,11 +4,20 @@ import sqlalchemy as sqlalchemy
 
 
 # https://www.bls.gov/cpi/data.htm
-df_explore = pd.read_csv(r"C:\git\bls\data\cu\cu.data.0.Current", sep="\t")
+df_explore = pd.read_csv(r"C:\git\bls\data\cu\cu.data.0.Current", sep="\t",nrows=100) # Adding the nrows to go faster. This is big and Pycharm doesn't like it.
 df_series = pd.read_csv(r"C:\git\bls\data\cu\cu.series", sep="\t")
-current_period = df_explore.query("year == 2021 & period =='M04'")
 
+df_obj = df_series.select_dtypes(['object'])
+df_explore_obj = df_explore.select_dtypes(['object'])
+# https://stackoverflow.com/questions/40950310/strip-trim-all-strings-of-a-dataframe
+df_series[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
+df_explore[df_explore_obj.columns] = df_explore_obj.apply(lambda x: x.str.strip())
 
+# https://stackoverflow.com/questions/21606987/how-can-i-strip-the-whitespace-from-pandas-dataframe-headers
+df_series.columns = df_series.columns.str.strip()
+df_explore.columns = df_explore.columns.str.strip()
+
+# current_period = df_explore.query("year == 2021 & period =='M04'")
 
 sqlite = sqlalchemy.pool.manage(sql, poolclass=sqlalchemy.pool.StaticPool )
 conn = sqlite.connect(':memory:')
